b60f90dae9c3b50e086afc0904392fefb7c401bc
cathy04 <catherinehouenou@gmail.com>
2025-05-12 13:18:10 +0100

    test

---
+++ src/app/tests/page.tsx
@@ -0,0 +1,148 @@
+'use client'
+
+export default function name() {
+  return (
+    <div className="">
+      <header className="bg-gradient-to-br from-violet-500 to-pink-400 h-52"></header>
+      <div className="relative bottom-16 container mx-8 ml-auto mr-auto  xl:w-6/12 flex flex-wrap items-start">
+        <div className="w-full grid xl:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-4">
+          <div className="bg-white w-full mx-auto border transform hover:translate-y-2 shadow-xl transition duration-300">
+            <div className="flex justify-end p-4">
+              <button type="button">...</button>
+            </div>
+
+            <img src="img/a1.svg" alt="" className="h-16 md:h-32 mx-auto object-contain" />
+
+            <h1 className="text-center text-2xl font-bold mt-4 mb-6">title</h1>
+
+            <div className="bg-amber-100 p-4">
+              <div className="mt-2 rounded-lg flex flex-wrap justify-between gap-4">
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">Projects</h6>
+                </div>
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">project</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-white w-full mx-auto border transform hover:translate-y-2 shadow-xl transition duration-300">
+            <div className="flex justify-end p-4">
+              <button type="button">...</button>
+            </div>
+
+            <img src="img/a1.svg" alt="" className="h-16 md:h-32 mx-auto object-contain" />
+
+            <h1 className="text-center text-2xl font-bold mt-4 mb-6">title</h1>
+
+            <div className="bg-amber-100 p-4">
+              <div className="mt-2 rounded-lg flex flex-wrap justify-between gap-4">
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">Projects</h6>
+                </div>
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">project</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-white w-full mx-auto border transform hover:translate-y-2 shadow-xl transition duration-300">
+            <div className="flex justify-end p-4">
+              <button type="button">...</button>
+            </div>
+
+            <img src="img/a1.svg" alt="" className="h-16 md:h-32 mx-auto object-contain" />
+
+            <h1 className="text-center text-2xl font-bold mt-4 mb-6">title</h1>
+
+            <div className="bg-amber-100 p-4">
+              <div className="mt-2 rounded-lg flex flex-wrap justify-between gap-4">
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">Projects</h6>
+                </div>
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">project</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-white w-full mx-auto border transform hover:translate-y-2 shadow-xl transition duration-300">
+            <div className="flex justify-end p-4">
+              <button type="button">...</button>
+            </div>
+
+            <img src="img/a1.svg" alt="" className="h-16 md:h-32 mx-auto object-contain" />
+
+            <h1 className="text-center text-2xl font-bold mt-4 mb-6">title</h1>
+
+            <div className="bg-amber-100 p-4">
+              <div className="mt-2 rounded-lg flex flex-wrap justify-between gap-4">
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">Projects</h6>
+                </div>
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">project</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-white w-full mx-auto border transform hover:translate-y-2 shadow-xl transition duration-300">
+            <div className="flex justify-end p-4">
+              <button type="button">...</button>
+            </div>
+
+            <img src="img/a1.svg" alt="" className="h-16 md:h-32 mx-auto object-contain" />
+
+            <h1 className="text-center text-2xl font-bold mt-4 mb-6">title</h1>
+
+            <div className="bg-amber-100 p-4">
+              <div className="mt-2 rounded-lg flex flex-wrap justify-between gap-4">
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">Projects</h6>
+                </div>
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">project</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-white w-full mx-auto border transform hover:translate-y-2 shadow-xl transition duration-300">
+            <div className="flex justify-end p-4">
+              <button type="button">...</button>
+            </div>
+
+            <img src="img/a1.svg" alt="" className="h-16 md:h-32 mx-auto object-contain" />
+
+            <h1 className="text-center text-2xl font-bold mt-4 mb-6">title</h1>
+
+            <div className="bg-amber-100 p-4">
+              <div className="mt-2 rounded-lg flex flex-wrap justify-between gap-4">
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">Projects</h6>
+                </div>
+                <div className="flex-1 min-w-[100px] text-center">
+                  <h5 className="font-bold">5</h5>
+                  <h6 className="text-gray-400">project</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
